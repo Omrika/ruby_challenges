0ca544f68662445473659cee862824bce4ca2b1d
Omrika Folkes <omrikafolkes@gmail.com>
2015-02-23 21:52:52 -0500

    refactored

--- class.rb
+++ class.rb
@@ -1,4 +1,4 @@
-class Dog
+class Animal
 	def set_name=(name)
 		@name = name
 	end
@@ -7,12 +7,54 @@ class Dog
 		return @name
 	end
 
-	def bark
-		return "#{@name} says hello in Dog, bark bark!"
+	def set_legs=(legs)
+		@legs = legs
+	end
+
+	def get_legs 
+		return @legs
 	end
 end
 
+class Dog < Animal
+	def bark
+		return "bark bark!"
+	end
+end
+
+class Duck < Animal
+	def quack
+		return "quack quack quack!"
+	end
+end
+
+class Cow < Animal
+	def moo
+		return "moooooooo!"
+	end
+end
 
 	my_Dog = Dog.new
 	my_Dog.set_name = "Chummy"
-	puts my_Dog.bark
+	Dog_name = my_Dog.get_name
+	my_Dog.set_legs = 4
+	Dog_legs = my_Dog.get_legs
+
+	my_Duck = Duck.new
+	my_Duck.set_name = "Duckie"
+	Duck_name = my_Duck.get_name
+	my_Duck.set_legs = 2
+	Duck_legs = my_Duck.get_legs
+
+	my_Cow = Cow.new
+	my_Cow.set_name = "Anna"
+	Cow_name = my_Cow.get_name
+	my_Cow.set_legs = 4
+	Cow_legs = my_Cow.get_legs
+
+
+	puts "My cow #{Cow_name} has #{Cow_legs} legs and says #{my_Cow.moo}. My dog #{Dog_name} has #{Dog_legs} legs and says #{my_Dog.bark}. My duck #{Duck_name} has #{Duck_legs} legs and says #{my_Duck.quack}! I love my animals!"
+
+	puts my_Dog.inspect
+	puts my_Duck.inspect
+	puts my_Cow.inspect
--- edit.rb
+++ edit.rb
@@ -1,90 +1,21 @@
-class Ferret
-
-	def set_name=(ferret_name)
-		@name = ferret_name
+class Animal
+	def set_name=(name)
+		@name = name
 	end
 
 	def get_name
 		return @name
 	end
-
-	def set_owner=(owner_name)
-		@owner_name = owner_name
-	end
-
-	def get_owner
-		return @owner_name
-	end
-
-	def squeal
-		return "squeeeeee"
-	end
-
 end
 
-class Chincilla
-
-	def set_name=(chincilla_name)
-		@name = chincilla_name
-	end
-
-	def get_name
-		return @name
-	end
-
-	def set_owner=(owner_name)
-		@owner_name = owner_name
-	end
-
-	def get_owner
-		return @owner_name
-	end
-
-	def squeek
-		return "eeeep"
-	end
-
-end
-
-class Parrot
-
-	def set_name=(parrot_name)
-		@name = parrot_name
-	end
-
-	def get_name
-		return @name
-	end
-
-	def set_owner=(owner_name)
-		@owner_name = owner_name
-	end
-
-	def get_owner
-		return @owner_name
-	end
-
-	def tweet
-			return "chirp chirp"
+class Dog < Animal
+	def bark
+		return "ruf ruf"
 	end
 end
 
-my_ferret = Ferret.new
-my_ferret.set_name= "Fredo"
-ferret_name = my_ferret.get_name
+my_Dog = Dog.new
+my_Dog.set_name = "chummy"
+Dog_name = my_Dog.get_name
 
-my_parrot = Parrot.new
-my_parrot.set_name= "Budgie"
-parrot_name = my_parrot.get_name
-
-my_chincilla = Chincilla.new
-my_chincilla.set_name= "Dali"
-chincilla_name = my_chincilla.get_name
-
-puts "#{ferret_name} says #{my_ferret.squeal}, 
-#{parrot_name} says #{my_parrot.tweet}, 
-and #{chincilla_name} says #{my_chincilla.squeek}."
-
-puts my_ferret.inspect
-puts my_parrot.inspect
-puts my_chincilla.inspect
+puts "My dog #{Dog_name} says #{my_Dog.bark}"
