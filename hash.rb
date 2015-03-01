e2b5957923374ec87750a3e42b4ed699e652c43a
Omrika Folkes <omrikafolkes@gmail.com>
2015-02-28 20:31:10 -0500

    symbols

---
+++ hash.rb
@@ -0,0 +1,8 @@
+entry_1 = {:name => "Omrika", :age => "20"}
+entry_2 = {:name => "Tikaa", :age => "23"}
+entry_3 = {:name => "Rika", :age => "15"}
+
+
+name = entry_1[:name]
+
+puts "#{name} is a good person."
