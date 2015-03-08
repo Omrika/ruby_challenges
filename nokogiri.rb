8d91d8e69beea453cd13e11b2b13d19b57c4448b
Omrika <Omrikafolkes@gmail.com>
2015-03-07 20:08:05 -0500

    recipes

--- nokogiri.rb
+++ nokogiri.rb
@@ -1,6 +1,10 @@
 require 'nokogiri' #need nokogiri to work
 require 'open-uri' #dependency nokogiri requires you to have.
 
-doc = Nokogiri::HTML(open('http://www.google.com')) #nokogiri go to this website and get html doc and bring it back and save it to the doc variable
+doc = Nokogiri::HTML(open("http://www.marthastewart.com/312598/brick-pressed-sandwich")) #nokogiri go to this website and get html doc and bring it back and save it to the doc variable
 
-puts doc.methods
+list = doc.css('.components-data')
+
+list.each do |x|
+	puts x.inner_html
+end
