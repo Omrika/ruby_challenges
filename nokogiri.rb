f14ef9f9656c225fedbe3e9f95a0bdf5c3d53915
Omrika <Omrikafolkes@gmail.com>
2015-03-07 19:39:44 -0500

    nokogiri testing

---
+++ nokogiri.rb
@@ -0,0 +1,6 @@
+require 'nokogiri' #need nokogiri to work
+require 'open-uri' #dependency nokogiri requires you to have.
+
+doc = Nokogiri::HTML(open('http://www.googlegit.com')) #nokogiri go to this website and get html doc and bring it back and save it to the doc variable
+
+puts doc
