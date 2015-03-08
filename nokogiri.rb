05cdb06ca059ffdd77f343cc8084d5ff85468a7c
Omrika <Omrikafolkes@gmail.com>
2015-03-07 19:46:18 -0500

    methods

--- nokogiri.rb
+++ nokogiri.rb
@@ -1,6 +1,6 @@
 require 'nokogiri' #need nokogiri to work
 require 'open-uri' #dependency nokogiri requires you to have.
 
-doc = Nokogiri::HTML(open('http://www.googlegit.com')) #nokogiri go to this website and get html doc and bring it back and save it to the doc variable
+doc = Nokogiri::HTML(open('http://www.google.com')) #nokogiri go to this website and get html doc and bring it back and save it to the doc variable
 
-puts doc
+puts doc.methods
