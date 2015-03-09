c6da231576cea329948e0605ebfbdc4795b8bbc7
Omrika <Omrikafolkes@gmail.com>
2015-03-09 11:00:09 -0400

    practice using gems

---
+++ twilio.rb
@@ -0,0 +1,45 @@
+require 'rubygems'
+require 'twilio-ruby'
+
+
+account_sid = 'My_account_sid'
+auth_token = 'My_auth_token'
+
+@client = Twilio::REST::Client.new(account_sid, auth_token)
+
+def twil
+	answer = ['It is certain',
+'It is decidedly so',
+'Without a doubt',
+'Yes definitely',
+'You may rely on it',
+'As I see it, yes',
+'Most likely',
+'Outlook good',
+'Yes',
+'Signs point to yes',
+'Reply hazy try again',
+'Ask again later',
+'Better not tell you now',
+'Cannot predict now',
+'Concentrate and ask again',
+'Don/t count on it',
+'My reply is no',
+'My sources say no',
+'Outlook not so good',
+'Very doubtful']
+
+response = answer.sample
+
+message = @client.account.messages.create(
+	:from => 'xxxx'
+	:to => 'xxxx'
+	:body => response
+)
+end
+
+puts "questions?"
+
+user_input = gets
+
+puts twil
