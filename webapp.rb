33718f3f6e70555761b7c14a26bc349871c92ea4
Omrika <Omrikafolkes@gmail.com>
2015-03-15 12:46:32 -0400

    numerology web app

--- webapp.rb
+++ webapp.rb
@@ -1,24 +1,50 @@
 require "sinatra"
-
-def determine_current_hour
-	current_time = Time.new
-	current_hour = current_time.hour
-end
  
-def greeting(name)
-	current_hour = determine_current_hour
-	
-	if(current_hour > 3 && current_hour < 12)
-		time = "morning"
-	elsif(current_hour > 12 && current_hour < 18)
-		time = "afternoon"
-	elsif(current_hour > 18 || current_hour < 2)
-		time = "evening"
+def user_bday (birthdate)
+	number = birthdate[0].to_i + birthdate[1].to_i + birthdate[2].to_i + birthdate[3].to_i + birthdate[4].to_i + birthdate[5].to_i + birthdate[6].to_i + birthdate[7].to_i
+ 
+	number = number.to_s
+	number = number[0].to_i + number[1].to_i
+	 
+	if number > 9
+	number = number[0].to_i + number[1].to_i
 	end
-	my_greeting = "Good #{time}, #{name.capitalize}!"
+	 
+	return number
+	end
+
+def get_msg (birth_num)
+	
+	case birth_num
+	when 1
+		message = 'One is the leader. The number one indicates the ability to stand alone, and is a strong vibration. Ruled by the Sun.'
+	when 2
+		message =  'This is the mediator and peace-lover. The number two indicates the desire for harmony. It is a gentle, considerate, and sensitive vibration. Ruled by the Moon.'
+	when 3
+		message = 'Number Three is a sociable, friendly, and outgoing vibration. Kind, positive, and optimistic, Three enjoy life and have a good sense of humor. Ruled by Jupiter.'
+	when 
+		message = 'This is the worker. Practical, with a love of detail, Fours are trustworthy, hard-working, and helpful. Ruled by Uranus.'
+	when 5
+	message =  'This is the freedom lover. The number five is an intellectual vibration. These are idea people with a love of variety and the ability to adapt to most situations. Ruled by Mercury.'
+	when 6
+		message = 'This is the peace lover. The number six is a loving, stable, and harmonious vibration. Ruled by Venus.'
+	when 7 
+		message = 'This is the deep thinker. The number seven is a spiritual vibration. These people are not very attached to material things, are introspective, and generally quiet. Ruled by Neptune.'
+	when  8
+		message =  'This is the manager. Number Eight is a strong, successful, and material vibration. Ruled by Saturn.'
+	when 9
+	message =  'This is the teacher. Number Nine is a tolerant, somewhat impractical, and sympathetic vibration. Ruled by Mars.'
+	else 
+		puts 'welp! your numerolgy number isnt 1-9'
+	end
 end
 
-get "/:name" do
-	name = params[:name].capitalize
-	my_greeting = greeting(name)
+
+
+
+get "/:birthdate" do
+	birthdate = params[:birthdate]
+	birth_num = user_bday(birthdate)
+	message = get_msg(birth_num)
+	"#{message}"
 end
