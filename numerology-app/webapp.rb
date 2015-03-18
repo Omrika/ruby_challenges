16422be523cf81c3b11c3243cfafe4efa0d65a83
Omrika <Omrikafolkes@gmail.com>
2015-03-17 21:41:59 -0400

    final code

--- numerology-app/webapp.rb
+++ numerology-app/webapp.rb
@@ -1,17 +1,17 @@
-require "sinatra"
- 
+require 'sinatra'
+
 def user_bday (birthdate)
 	number = birthdate[0].to_i + birthdate[1].to_i + birthdate[2].to_i + birthdate[3].to_i + birthdate[4].to_i + birthdate[5].to_i + birthdate[6].to_i + birthdate[7].to_i
- 
+	
 	number = number.to_s
 	number = number[0].to_i + number[1].to_i
-	 
+
 	if number > 9
-	number = number[0].to_i + number[1].to_i
+		number =number[0].to_i + number[1].to_i
 	end
-	 
+
 	return number
-	end
+end
 
 def get_msg (birth_num)
 	
@@ -21,8 +21,8 @@ def get_msg (birth_num)
 	when 2
 		message =  'This is the mediator and peace-lover. The number two indicates the desire for harmony. It is a gentle, considerate, and sensitive vibration. Ruled by the Moon.'
 	when 3
-		message = 'Number Three is a sociable, friendly, and outgoing vibration. Kind, positive, and optimistic, Three enjoy life and have a good sense of humor. Ruled by Jupiter.'
-	when 
+		message = 'Number Three is a sociable, friendly, and outgoing vibration. Kind, positive, and optimistic, Threes enjoy life and have a good sense of humor. Ruled by Jupiter.'
+	when 4
 		message = 'This is the worker. Practical, with a love of detail, Fours are trustworthy, hard-working, and helpful. Ruled by Uranus.'
 	when 5
 	message =  'This is the freedom lover. The number five is an intellectual vibration. These are idea people with a love of variety and the ability to adapt to most situations. Ruled by Mercury.'
@@ -40,12 +40,20 @@ def get_msg (birth_num)
 end
 
 
-
-
-get "/:birthdate" do
+get '/:birthdate' do
 	birthdate = params[:birthdate]
 	birth_num = user_bday(birthdate)
 	@message = get_msg(birth_num)
-	
+	erb :index
+end
+
+get '/' do
+  erb :form
+end
+
+post '/' do
+	birthdate = params[:birthdate]
+	birth_num = user_bday(birthdate)
+	@message = get_msg(birth_num)
 	erb :index
 end
